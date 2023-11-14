75901b59efaa9ff56d3c932b5d1981dcf06e1558
amaamal-dev-01 <devmpamal@gmail.com>
2023-11-14 11:43:36 +0530

    Modify the todo

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import counterSlice from "../features/counterSlice";
 import formSlice from "../features/formSlice";
 
 
 
 const store = configureStore({
     reducer:{
-        // counter :counterSlice,
         task:formSlice
     }
 })
